82c3f0717e4ab5d437c092f1069161cc601d3b6b
Zhongdong Yang <zhongdong_y@outlook.com>
2023-04-29 11:08:08 +0800

    Add: database helper macros

---
+++ rustybot-macros/src/lib.rs
@@ -0,0 +1,16 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+/// This procedural macro will introduce a mutable
+/// [`PoolConnection`][`sqlx::pool::PoolConnection`] instance called
+/// `connection`. So you can just operate on that. Such as making queries.
+#[proc_macro]
+pub fn get_connection(_item: TokenStream) -> TokenStream {
+    quote! {
+      use crate::DB_POOL;
+      let pool_guard = DB_POOL.lock().unwrap();
+      let mut connection = pool_guard.as_ref().unwrap().acquire().await.unwrap();
+      drop(pool_guard);
+    }
+    .into()
+}
